b0d4c24f7da2c550be6982c6dc9150003da3746f
manulep <emmanuel.lepevedic.pro@gmail.com>
2022-12-09 16:42:22 +0100

    demarrage

---
+++ project/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from flask_bootstrap import Bootstrap5
+
+# configuration
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+"""
+if app.config['DEBUG']:
+    print("Database : " + app.config['SQLALCHEMY_DATABASE_URI'])
+
+# config sqlalchemy
+db = SQLAlchemy(app)
+
+# bootstrap 5
+bootstrap = Bootstrap5(app)
+
+# page d'accueil
+@app.get('/')
+def home():
+    return render_template('index.html')
+
+# routes pour student
+from .controllers import student_controller
+app.register_blueprint(student_controller.student_app, url_prefix='/student')
+
+"""
---
+++ project/config.py
@@ -0,0 +1,14 @@
+"""Flask configuration."""
+from os import environ
+from dotenv import load_dotenv
+
+# valeurs par defaut
+environ.setdefault('SESSION_COOKIE_HTTPONLY', "true")
+
+# var env dans fichier .env
+load_dotenv(override=False)
+SECRET_KEY = environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+SESSION_COOKIE_HTTPONLY = environ.get('SESSION_COOKIE_HTTPONLY')
+REMEMBER_COOKIE_HTTPONLY = environ.get('REMEMBER_COOKIE_HTTPONLY')
