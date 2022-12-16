7b5fd5e8b02e25c0b56d8aa4449ce2c0e8f67442
manulep <emmanuel.lepevedic.pro@gmail.com>
2022-12-16 10:17:57 +0100

    add delete film

--- project/controllers/film_controller.py
+++ project/controllers/film_controller.py
@@ -1,6 +1,9 @@
-# routes
-from flask import Blueprint, render_template
+''' 
+    routes pour l'entité Film
+'''
+from flask import Blueprint, render_template, Response, redirect, url_for
 from project.models.film import Film
+from project import db
 
 film_app = Blueprint('film_func', __name__)
 
@@ -14,3 +17,9 @@ def display(film_id: int) -> str:
     film = Film.query.get_or_404(film_id)
     return render_template('film/detail.html', film=film)
 
+@film_app.post('/<int:film_id>/delete/')
+def delete(film_id: int) -> Response:
+    film = Film.query.get_or_404(film_id)
+    db.session.delete(film)
+    db.session.commit()
+    return redirect(url_for('film_func.list'))
